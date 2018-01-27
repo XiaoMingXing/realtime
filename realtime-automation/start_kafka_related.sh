cec09aaa2073826b721926830d9334817337bd84
xiaomingxing <mxxiao@thoughtworks.com>
2018-01-27 19:48:24 +0800

    [Automation] [Mingxing] suport to start kafka related thingsˆ

---
+++ realtime-automation/start_kafka_related.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# start business system
+export PROJECT_HOME=~/projects/realtime
+export CONFLUENT_HOME=~/projects/confluent-4.0.0
+
+
+#start confluent kafka rest
+run_kafka_rest(){
+     echo "Run Kafka Rest ..."
+     cd ${CONFLUENT_HOME}
+     bin/kafka-rest-start etc/kafka-rest/kafka-rest.properties &
+}
+
+# run kafka streams transformation
+run_kafka_streams(){
+     echo "Run Kafka Streams ..."
+     cd ${PROJECT_HOME}/realtime-kafka-streams
+     mvn clean package
+     cd target
+     java -cp realtime-kafka-streams-1.0-SNAPSHOT-jar-with-dependencies.jar com.everydots.kafka.streams.Transformer &
+}
+
+
+
+# Run kafka connector
+run_kafka_connector(){
+    echo "Run Kafka Connector ..."
+    #build connector package
+    cd ${PROJECT_HOME}/realtime-kafka-connectors
+    mvn clean package
+
+    #run the connector
+    ${CONFLUENT_HOME}/bin/connect-standalone config/worker.properties config/connect.properties &
+}
+
+run_kafka_rest
+run_kafka_streams
+run_kafka_connector
