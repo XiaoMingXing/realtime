d57bdb5784c58e74a28d61151abb3549990eaf74
davidtan <davidtan@thoughtworks.com>
2018-01-31 15:22:59 +0800

    rename directory to pubsub

--- realtime-clouds/app/pubsub/subscribe_bitcoin_price.py
+++ realtime-clouds/app/pubsub/subscribe_bitcoin_price.py
@@ -1,9 +1,9 @@
 import time
-from google.cloud import pubsub_v1
+from google.cloud import pubsub
 
 def receive_messages(project, subscription_name):
     """Receives messages from a pull subscription."""
-    subscriber = pubsub_v1.SubscriberClient()
+    subscriber = pubsub.SubscriberClient()
     subscription_path = subscriber.subscription_path(
         project, subscription_name)
 
