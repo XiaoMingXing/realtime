d4e8ebdf24a1df6ecc76c30b533af4618bd2dcf9
xiaomingxing <mxxiao@thoughtworks.com>
2018-02-09 12:33:31 +0800

    [Ming Xing] change the config socket.io addressπ

--- realtime-dashboard/components/socket.js
+++ realtime-dashboard/components/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client"
 
-let BACKEND_URL = "http://localhost:8080";
+let BACKEND_URL = "http://35.198.201.186:8080";
 let socket = io(BACKEND_URL);
 
 export default socket
