2c85da7080da26bd9fa482066d1eedad37bbb922
xiaomingxing <mxxiao@thoughtworks.com>
2018-02-11 19:26:32 +0800

    [Ming Xing] support forπ get current ip address

--- realtime-dashboard/components/socket.js
+++ realtime-dashboard/components/socket.js
@@ -1,6 +1,10 @@
 import io from "socket.io-client"
+import ip from "ip"
 
-let BACKEND_URL = "http://localhost:8089";
+let current_ip = ip.address();
+console.log("CURRENT IP:", current_ip);
+let BACKEND_URL = "http://" + current_ip + ":8089";
 let socket = io(BACKEND_URL);
+
 
 export default socket
